5d925070bcd2485039613b878071bd037140ef38
Master-mohit <mohitmansigupta9098@gmail.com>
2025-05-15 22:49:37 +0530

    tudo list

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+const [task, settask] = useState('')
+const [des, setdes] = useState('')
+const [newtask, setnewtask] = useState([])
+const [edit, setedit] = useState(null)
+
+
+  const submithandler = (e) => {
+  e.preventDefault()
+  if(task == '' || des == ''){
+    alert('Please fill all the fields')
+  }
+  
+  const data = {task, des}
+  if(edit != null){
+    let updatetask = [...newtask]
+
+    updatetask[edit] = data
+    setnewtask(updatetask)
+    setedit(null)
+  }
+  else{
+    setnewtask([...newtask, data])
+  }
+  settask('')
+  setdes('')
+
+}
+
+
+const deletehandler = (i) => {
+  let copytask = [...newtask]
+  copytask.splice(i, 1)
+  setnewtask(copytask)
+}
+
+
+const  edithandler = (i) => {
+  setedit(i)
+  settask(newtask[i].task)
+  setdes(newtask[i].des)
+  
+
+}
+
+  return (
+    <div className=' w-screen h-screen bg-zinc-900 text-2xl font-bold px-6 py-4 text-white'>
+      <form onSubmit={submithandler}>
+            <input value={task} onChange={(e) => {
+              settask(e.target.value)
+            }} className='border-2 text-white px-4 py-2 m-4 rounded border-amber-100 ' 
+            type='text' placeholder='Enter task Name'/>
+
+             <input value={des} onChange={(e) => {
+              setdes(e.target.value)
+             }} className='border-2 text-white px-4 py-2 m-4 rounded border-amber-100 ' 
+            type='text' placeholder='Enter Description'/>
+
+             <input className='border-2 rounded border-emerald-100 px-4 py-2 mx-4 font-semibold' type='submit'/>
+      </form>
+
+ <div className='w-50 m-4 gap-4 px-4 py-2 flex items-center justify-center flex-col text-center'>
+  {newtask.length > 0 ? (
+    newtask.map((t, i)=> {
+    return (
+      <div key={i} className='border-2 border-emerald-100 rounded px-4 py-2 m-4'>
+        <h1 className='text-xl font-bold'>{t.task}</h1>
+        <p>{t.des}</p>
+
+        <div className='flex items-center justify-center'>
+          <button onClick={() =>{
+            deletehandler(i)
+          }} className='bg-red-400 text-white p-2 m-4 rounded font-semibold text-[20px]'> Delete </button>
+        </div>
+
+        <div>
+          <button onClick={() => {
+            edithandler(i)
+          }} className='px-5 py-2 bg-black text-white rounded font-semibold'>Edit</button>
+        </div>
+      </div>
+    ) 
+   })
+  ) : (
+    <h1 className='text-2xl font-bold'>No Task Found</h1>
+  )}
+ </div>
+  
+    </div>
+  )
+}
+
+export default App
